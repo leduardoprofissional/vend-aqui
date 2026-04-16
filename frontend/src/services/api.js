6cb4de0b386d53b7deb490639a69b47327cad2ca
leduardoprofissional <leduardoprofissional@gmail.com>
2026-04-16 17:21:54 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { getUser } from "./auth";
 
 const api = axios.create({
   baseURL: "https://vend-aqui.onrender.com"
+});
+
+api.interceptors.request.use(config => {
+  const user = getUser();
+
+  if (user?.token) {
+    config.headers.Authorization = user.token;
+  }
+
+  return config;
 });
 
 export default api;
