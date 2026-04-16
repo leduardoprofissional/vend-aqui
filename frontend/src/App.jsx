c0c8058c9885c4c6c59bf9d446f85388da79f269
leduardoprofissional <leduardoprofissional@gmail.com>
2026-04-16 15:06:34 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateAd from "./pages/CreateAd";
 
 function App() {
   return (
     <BrowserRouter>
+      <nav style={{
+        background: "#ff6600",
+        padding: 15,
+        display: "flex",
+        justifyContent: "space-between"
+      }}>
+        <Link to="/" style={{ color: "#fff" }}>Home</Link>
+        <Link to="/create" style={{ color: "#fff" }}>Anunciar</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateAd />} />
